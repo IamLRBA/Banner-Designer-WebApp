7708d4af5d14d0283468a0abd14e2f94890fc55b
Jerry Laruba Festus <jerrylarubafestus@gmail.com>
2025-04-30 12:15:46 +0300

    Banner Size Control

--- src/components/InteractiveBanner.jsx
+++ src/components/InteractiveBanner.jsx
@@ -330,3 +330,76 @@ import {
             </div>
           )}
         </div>
+
+        {/* Banner Size Control section */}
+        <div className="tool-section">
+          <label>
+            <FaArrowsAlt /> Banner Size
+          </label>
+
+          {/* Width Control */}
+          <div className="control-group">
+            <label>Width: {width}px</label>
+            <input
+              type="range"
+              min="100"
+              max="1200"
+              value={width}
+              onChange={(e) => setWidth(parseInt(e.target.value))}
+            />
+            <div className="button-group">
+              <button
+                onClick={() => setWidth((prev) => Math.max(100, prev - 50))}
+              >
+                <FaMinus />
+              </button>
+              <button onClick={() => setWidth((prev) => prev + 50)}>
+                <FaPlus />
+              </button>
+            </div>
+          </div>
+
+          {/* Height Control */}
+          <div className="control-group">
+            <label>Height: {height}px</label>
+            <input
+              type="range"
+              min="50"
+              max="800"
+              value={height}
+              onChange={(e) => setHeight(parseInt(e.target.value))}
+            />
+            <div className="button-group">
+              <button
+                onClick={() => setHeight((prev) => Math.max(50, prev - 50))}
+              >
+                <FaMinus />
+              </button>
+              <button onClick={() => setHeight((prev) => prev + 50)}>
+                <FaPlus />
+              </button>
+            </div>
+          </div>
+
+          {/* Border Radius Control */}
+          <div className="control-group">
+            <label>Border Radius: {borderRadius}px</label>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={borderRadius}
+              onChange={(e) => setBorderRadius(parseInt(e.target.value))}
+            />
+            <div className="button-group">
+              <button
+                onClick={() => setBorderRadius((prev) => Math.max(0, prev - 5))}
+              >
+                <FaMinus />
+              </button>
+              <button onClick={() => setBorderRadius((prev) => prev + 5)}>
+                <FaPlus />
+              </button>
+            </div>
+          </div>
+        </div>
