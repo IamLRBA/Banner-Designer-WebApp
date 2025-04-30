aa4df08c3d433e8df95238b33e2f7fe933f28149
Jerry Laruba Festus <jerrylarubafestus@gmail.com>
2025-04-30 12:07:27 +0300

    update

--- src/components/InteractiveBanner.jsx
+++ src/components/InteractiveBanner.jsx
@@ -2,3 +2,25 @@ import React, { useState } from "react";
 import Banner from "./Banner";
 import { motion } from "framer-motion";
 import { SketchPicker } from "react-color";
+import {
+    FaUpload,
+    FaEyeDropper,
+    FaTextHeight,
+    FaTools,
+    FaShapes,
+    FaPlus,
+    FaMinus,
+    FaTable,
+    FaIcons,
+    FaHighlighter,
+    FaPaintBrush,
+    FaGripLines,
+    FaSmile,
+    FaPen,
+    FaEraser,
+    FaCog,
+    FaChevronDown,
+    FaArrowsAlt,
+  } from "react-icons/fa";
+  import "../App.css";
+  import "../index.css";
