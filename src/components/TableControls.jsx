cebe180f4591e1455540964ea019608ac7f791cd
IamLRBA <jerrylarubafestus@gmail.com>
2025-05-21 06:02:41 -0700

    Updated TableControls.jsx

--- src/components/TableControls.jsx
+++ src/components/TableControls.jsx
@@ -1,10 +1,64 @@
 import React from "react";
+import { FaTable, FaPlus, FaMinus, FaChevronDown } from "react-icons/fa";
 
-const TableControls = () => {
+const TableControls = ({
+  showTableOptions,
+  setShowTableOptions,
+  showAddOptions,
+  setShowAddOptions,
+  showRemoveOptions,
+  setShowRemoveOptions,
+}) => {
   return (
-    <div>
-      <p>Table Controls Component</p>
-    </div>
+    <>
+      <label onClick={() => setShowTableOptions(!showTableOptions)}>
+        Table <FaChevronDown />
+      </label>
+      {showTableOptions && (
+        <div className="dropdown">
+          <div className="tooltip-container">
+            <button className="table-btn">
+              <FaTable />
+            </button>
+            <span className="tooltip">Create</span>
+          </div>
+          <div className="dropdown-container">
+            <div className="tooltip-container">
+              <button
+                className="table-btn"
+                onClick={() => setShowAddOptions(!showAddOptions)}
+              >
+                <FaPlus />
+              </button>
+              <span className="tooltip">Add</span>
+            </div>
+            {showAddOptions && (
+              <div className="sub-dropdown">
+                <button>Add Row</button>
+                <button>Add Column</button>
+              </div>
+            )}
+          </div>
+          <div className="dropdown-container">
+            <div className="tooltip-container">
+              <button
+                className="table-btn"
+                onClick={() => setShowRemoveOptions(!showRemoveOptions)}
+              >
+                <FaMinus />
+              </button>
+              <span className="tooltip">Remove</span>
+            </div>
+            {showRemoveOptions && (
+              <div className="sub-dropdown">
+                <button>Remove Row</button>
+                <button>Remove Column</button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
